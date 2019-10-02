f0d7ce4c92879ec320a5071006a51c425b18651c
Anılcan <igoodie@programmer.net>
2019-10-02 15:33:11 +0300

    fix(entity): loot generation ignoring size multiplier

--- src/main/java/kaptainwutax/itraders/entity/EntityFighter.java
+++ src/main/java/kaptainwutax/itraders/entity/EntityFighter.java
@@ -38,73 +38,69 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import scala.reflect.internal.Trees.This;
 
 public class EntityFighter extends EntityZombie {
-	
+
 	public AtomicReference<GameProfile> gameProfile = new AtomicReference<GameProfile>();
 	public AtomicReference<NetworkPlayerInfo> playerInfo = new AtomicReference<NetworkPlayerInfo>();
-	
+
 	public String lastName = "Fighter";
-	
+
 	public float sizeMultiplier = this.rand.nextFloat() * 2.0f + 0.8f;
 	public TieredLoot loot = TieredLoot.get(TieredLoot.Tier.COMMON);
-	
+
 	public EntityFighter(World world) {
-		super(world);			
-		this.setCustomNameTag(lastName);			
-		
+		super(world);
+		this.setCustomNameTag(lastName);
+
 		if(!this.world.isRemote) {
 			this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED)
-					.setBaseValue(this.rand.nextFloat() * 0.2d + 0.22d);	
-			
+					.setBaseValue(this.rand.nextFloat() * 0.2d + 0.22d);
+
 			 this.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(1.0D);
-			 
+
 			 this.loot = this.getFromSize(this.sizeMultiplier);
 		} else {
 			this.sizeMultiplier = 0.0f;
 		}
 	}
-	
+
 	public TieredLoot getFromSize(float m) {
 		if(m <= 1.0f)return TieredLoot.get(TieredLoot.Tier.COMMON);
 		else if(m <= 2.5f)return TieredLoot.get(TieredLoot.Tier.RARE);
 		else return TieredLoot.get(TieredLoot.Tier.EPIC);
 	}
-	
+
 	@Override
 	protected boolean canDespawn() {
 		return false;
 	}
-	
+
 	@Override
 	public boolean canPickUpLoot() {
 		return true;
 	}
-	
+
 	@Override
     protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty) {
         super.setEquipmentBasedOnDifficulty(difficulty);
-        
-        for(EntityEquipmentSlot s: EntityEquipmentSlot.values()) {
-        	this.setItemStackToSlot(s, this.loot.enchant(this.loot.getRandomLoot(s)));
-        }
     }
-	
+
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
 		if(this.isDead)return;
-		
+
 		if(this.world.isRemote) {
 			String name = this.getCustomNameTag();
-			
+
 			if(!lastName.equals(name)) {
 				this.updateSkin(name);
 			}
-		}	
-		
+		}
+
 		if(!this.world.isRemote && this.ticksExisted % 5 == 0) {
 			InitPacket.PIPELINE.sendToAllTracking(new S2CFighterHeight(this), this);
 		}
-		
+
 		if(this.moveForward > 0.2f) {
 			this.setSprinting(true);
 			this.getJumpHelper().setJumping();
@@ -112,108 +108,116 @@ public class EntityFighter extends EntityZombie {
 			this.setSprinting(false);
 		}
 	}
-	
+
 	@Override
 	public void writeEntityToNBT(NBTTagCompound compound) {
-		super.writeEntityToNBT(compound);		
+		super.writeEntityToNBT(compound);
 		compound.setFloat("SizeMultiplier", this.sizeMultiplier);
 	}
-	
+
 	@Override
 	public void readEntityFromNBT(NBTTagCompound compound) {
-		super.readEntityFromNBT(compound);		
-		if(compound.hasKey("SizeMultiplier"))this.changeSize(compound.getFloat("SizeMultiplier"));	
+		super.readEntityFromNBT(compound);
+
+		// Set up entity size, if present
+		if(compound.hasKey("SizeMultiplier"))
+			this.changeSize(compound.getFloat("SizeMultiplier"));
+
+		// Equip with loot
+		for(EntityEquipmentSlot s: EntityEquipmentSlot.values()) {
+			this.setItemStackToSlot(s, this.loot.enchant(this.loot.getRandomLoot(s)));
+		}
 	}
-	
+
 	public void changeSize(float m) {
 		if(m == this.sizeMultiplier)return;
 		this.sizeMultiplier = m;
 		this.setSize(0.6F * m, 1.95F * m);
 		this.multiplySize(1);
-		
+
 		if(!this.world.isRemote) {
 			this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH)
-				.setBaseValue(this.rand.nextInt(91) + (10 * m));	
+				.setBaseValue(this.rand.nextInt(91) + (10 * m));
 			this.setHealth(this.getMaxHealth());
 		}
-		
+
 		this.loot = this.getFromSize(this.sizeMultiplier);
 	}
-	
+
 	@Override
 	public float getEyeHeight() {
 		return 1.74f * this.sizeMultiplier;
 	}
-	
+
 	@Override
 	public void setChild(boolean childZombie) {
 		return;
 	}
-	
+
 	@Override
 	protected boolean shouldBurnInDay() {
 		return false;
 	}
-	
+
 	@Override
 	public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, IEntityLivingData livingdata) {
 		IEntityLivingData livingdata1 = super.onInitialSpawn(difficulty, livingdata);
-		this.setCustomNameTag(this.getCustomNameTag());	
+		this.setCustomNameTag(this.getCustomNameTag());
 		return livingdata1;
 	}
 
 	public void updateSkin(String name) {
-		if(!this.world.isRemote)return;		
+		if(!this.world.isRemote)return;
 		gameProfile.set(new GameProfile(null, name));
 		gameProfile.set(TileEntitySkull.updateGameprofile(gameProfile.get()));
 		playerInfo.set(new NetworkPlayerInfo(gameProfile.get()));
 		this.lastName = name;
 	}
-	
+
 	@SideOnly(value = Side.CLIENT)
     public ResourceLocation getLocationSkin() {
     	if(this.playerInfo == null || this.playerInfo.get() == null) {
     		return DefaultPlayerSkin.getDefaultSkinLegacy();
-    	}		
-    	
+    	}
+
         return this.playerInfo.get().getLocationSkin();
     }
-	
+
 	@Override
 	protected void dropEquipment(boolean wasRecentlyHit, int lootingModifier) {
 		super.dropEquipment(wasRecentlyHit, lootingModifier);
 		if(this.world.isRemote)return;
-		
+
 		List<Product> drops = InitConfig.CONFIG_FIGHTER.LOOT.stream()
 				.filter(product -> product.isValid()).collect(Collectors.toList());
 
 		Collections.shuffle(drops);
-	
+
 		for(int i = 0; i < Math.min(drops.size(), InitConfig.CONFIG_FIGHTER.LOOT_COUNT); i++) {
 			Product drop = drops.get(i);
 			this.entityDropItem(drop.toStack(), 0.0F);
 		}
-		
+
 		if(this.getCustomNameTag() != this.lastName) {
 			ItemStack headDrop = new ItemStack(Items.SKULL, 1, 3);
 			NBTTagCompound nbt = new NBTTagCompound();
 			nbt.setString("SkullOwner", this.getCustomNameTag());
 			headDrop.setTagCompound(nbt);
 			this.entityDropItem(headDrop, 0.0F);
-		}		
+		}
 	}
-	
+
 	@Override
 	public boolean attackEntityAsMob(Entity entity) {
 		if(!this.world.isRemote) {
 			((WorldServer)this.world).spawnParticle(
 					EnumParticleTypes.SWEEP_ATTACK,
-					this.posX, this.posY, this.posZ, 
+					this.posX, this.posY, this.posZ,
 					1, 0.0f, 0.0f, 0.0f, 0
 			);
 		}
-		
+
 		return super.attackEntityAsMob(entity);
 	}
-	
+
 }
