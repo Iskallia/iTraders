e4170e4fc918cef59d7601977b1d076515649a14
Unknown <kaptainwutax@gmail.com>
2019-10-15 19:25:58 -0400

    added inventory and mining distance

--- src/main/java/kaptainwutax/itraders/entity/EntityMiner.java
+++ src/main/java/kaptainwutax/itraders/entity/EntityMiner.java
@@ -4,25 +4,40 @@ import com.mojang.authlib.GameProfile;
 
 import kaptainwutax.itraders.SkinProfile;
 import kaptainwutax.itraders.net.FakeDigManager;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class EntityMiner extends EntityCreature {
-	
-	public FakeUser fakePlayer;
+
 	public final SkinProfile skin = new SkinProfile();	
-	public FakeDigManager digManager;
 	private String lastName = "Miner";
 	
+	public FakeUser fakePlayer;
+	public FakeDigManager digManager;
 	private EnumFacing miningDirection;
+	
+	private ItemStackHandler minerInventory = new ItemStackHandler(3 * 9 - 1);
+	private BlockPos startMiningPosition;
+	private int miningDistance;
 	
 	public EntityMiner(World world) {
 		super(world);
@@ -36,12 +51,29 @@ public class EntityMiner extends EntityCreature {
 	}
 	
 	@Override
-	public void onItemPickup(Entity entity, int quantity) {
-		if(entity instanceof EntityItem) {
-			EntityItem entityItem = (EntityItem)entity;
-		}
-		
-		super.onItemPickup(entity, quantity);
+    protected void updateEquipmentIfNeeded(EntityItem itemEntity) {
+        if(world.isRemote)return;
+        ItemStack itemstack = itemEntity.getItem();
+
+        for(int i = 0; i < this.minerInventory.getSlots() && !itemstack.isEmpty(); i++) {
+        	itemstack = this.minerInventory.insertItem(i, itemstack, false);
+        }
+        
+        if(itemEntity.getItem().getCount() == itemstack.getCount())return;
+        
+        if(itemstack.isEmpty()) {
+            itemEntity.setDead();
+        } else {
+        	itemEntity.getItem().setCount(itemstack.getCount());
+        }
+        
+        this.world.playSound(null, this.posX, this.posY, this.posZ, SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.NEUTRAL, 0.2F, (this.rand.nextFloat() - this.rand.nextFloat()) * 1.4F + 2.0F);
+    }
+	
+	@Override
+	public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, IEntityLivingData livingdata) {
+		this.startMiningPosition = this.getPosition();
+		return super.onInitialSpawn(difficulty, livingdata);
 	}
 	
 	@Override
@@ -61,10 +93,19 @@ public class EntityMiner extends EntityCreature {
 		if(!world.isRemote) {    
 			this.handleMining();
 		}	
-				
+		
+		this.updateArmSwingProgress();
 	}
 	
 	private void handleMining() {
+		double distanceSq = this.getPosition().distanceSq(this.startMiningPosition);
+		
+		if(distanceSq > this.miningDistance * this.miningDistance) {
+			this.onKillCommand();
+		}
+		
+		if(this.dead)return;
+		
 		if(this.miningDirection == null) {
 			this.miningDirection = EnumFacing.Plane.HORIZONTAL.random(this.rand);
 		}
@@ -75,14 +116,15 @@ public class EntityMiner extends EntityCreature {
 			BlockPos pos = this.getPosition().up(i).offset(this.miningDirection);
 			
 			if(!this.world.isAirBlock(pos)) {
-				this.digManager.onPlayerDamageBlock(pos, EnumFacing.NORTH);
+				this.digManager.onPlayerDamageBlock(pos, EnumFacing.NORTH);			
+				this.swingArm(EnumHand.MAIN_HAND);
 				mining = true;
 				break;
 			}
 		}	
 		
 		if(!mining) {
-			this.navigator.setPath(this.navigator.getPathToPos(this.getPosition().offset(this.miningDirection)), 0.5f);
+			this.navigator.setPath(this.navigator.getPathToPos(this.getPosition().offset(this.miningDirection, 1)), 0.5f);
 		}			
 	}
 
@@ -93,6 +135,10 @@ public class EntityMiner extends EntityCreature {
 		if(this.miningDirection != null) {
 			compound.setInteger("MiningDirection", this.miningDirection.ordinal());
 		}
+		
+		compound.setTag("MinerInventory", this.minerInventory.serializeNBT());		
+		compound.setLong("StartMiningPosition", this.startMiningPosition.toLong());
+		compound.setInteger("MiningDistance", this.miningDistance);
 	}
 	
 	@Override
@@ -102,6 +148,12 @@ public class EntityMiner extends EntityCreature {
 		if(compound.hasKey("MiningDirection")) {
 			this.miningDirection = EnumFacing.VALUES[compound.getInteger("MiningDirection")];
 		}
+		
+		this.minerInventory.deserializeNBT(compound.getCompoundTag("MinerInventory"));
+		this.startMiningPosition = BlockPos.fromLong(compound.getLong("StartMiningPosition"));
+		this.miningDistance = compound.getInteger("MiningDistance");
+		
+		if(this.miningDistance == 0)this.setMiningDistance(compound.getCompoundTag("SubData").getInteger("Months"));
 		
 		this.fakePlayer.setHeldItem(EnumHand.MAIN_HAND, this.getHeldItemMainhand());	
 	}
@@ -117,4 +169,48 @@ public class EntityMiner extends EntityCreature {
 		this.miningDirection = facing;	
 	}
 
+	@Override
+	public void onDeath(DamageSource cause) {	
+		if(!this.world.isRemote) {
+			this.world.setBlockState(this.getPosition(), Blocks.CHEST.getDefaultState());
+			TileEntity tileEntity = world.getTileEntity(this.getPosition());
+			
+			if(tileEntity instanceof TileEntityChest) {
+				TileEntityChest chestTileEntity = (TileEntityChest)tileEntity;
+				IItemHandler itemHandler = chestTileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
+				
+				int lastIndex = 0;
+				
+				for(int i =  0; i < this.minerInventory.getSlots(); i++) {
+					ItemStack stackInSlot = this.minerInventory.getStackInSlot(i);		
+					if(stackInSlot.isEmpty())break;
+					itemHandler.insertItem(i, stackInSlot, false);
+					lastIndex = i;
+				}			
+				
+				ItemStack pickaxe = this.getHeldItem(EnumHand.MAIN_HAND);
+				
+				if(!pickaxe.isEmpty() && itemHandler.getSlots() > lastIndex + 1) {
+					itemHandler.insertItem(lastIndex + 1, pickaxe, false);
+				}
+			}		
+		}
+		
+		super.onDeath(cause);
+	}
+	
+	public void setMiningDistance(int subMonths) {
+		if(subMonths < 3) {
+			this.miningDistance = 10 + this.rand.nextInt(25);
+		} else if(subMonths < 6) {
+			this.miningDistance = 10 + this.rand.nextInt(55);
+		} else if(subMonths < 12) {
+			this.miningDistance = 10 + this.rand.nextInt(85);
+		} else if(subMonths < 24) {
+			this.miningDistance = 10 + this.rand.nextInt(121);
+		} else {
+			this.miningDistance = 10 + this.rand.nextInt(151);
+		}
+	}
+	
 }
