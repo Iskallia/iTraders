c1621af9ba96f61b59a44ea13bb6458e5090f387
Unknown <kaptainwutax@gmail.com>
2019-10-16 18:27:05 -0400

    bump to 0.2.17

--- src/main/java/kaptainwutax/itraders/Traders.java
+++ src/main/java/kaptainwutax/itraders/Traders.java
@@ -17,7 +17,7 @@ public class Traders {
 
 	public static final String MOD_ID = "itraders";
 	public static final String MOD_NAME = "iTraders";
-	public static final String MOD_VERSION = "0.2.16";
+	public static final String MOD_VERSION = "0.2.17";
 
 	public static Traders getInstance() {
 		return INSTANCE;
