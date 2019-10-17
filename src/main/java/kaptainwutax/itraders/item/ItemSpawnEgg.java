166507515313985bd5d147eaa9b827c05deef79d
MCRcortex <18544518+MCRcortex@users.noreply.github.com>
2019-10-17 10:51:07 +1000

    Fixed names?

--- src/main/java/kaptainwutax/itraders/item/ItemSpawnEgg.java
+++ src/main/java/kaptainwutax/itraders/item/ItemSpawnEgg.java
@@ -221,12 +221,12 @@ public abstract class ItemSpawnEgg<T> extends Item {
 	}
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag isAdvanced) {
-		tooltip.set(0,UserNameChecker.GetTextFormattingFromItem(stack)+tooltip.get(0));
+		tooltip.set(0, UserNameChecker.getTextFormattingFromItem(stack)+tooltip.get(0));
 	}
 	@Override
 	public String getHighlightTip( ItemStack item, String displayName )
 	{
-		return UserNameChecker.GetTextFormattingFromItem(item)+displayName;
+		return UserNameChecker.getTextFormattingFromItem(item)+displayName;
 	}
 
 }
