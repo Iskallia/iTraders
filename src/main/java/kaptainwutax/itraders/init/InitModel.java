5dcdbc07b73e7232283e05e6c9848ad76eb9dcdc
Anılcan <igoodie@programmer.net>
2019-10-18 22:18:48 +0300

    feat(baubles): add drained texture for Skull Necklace

--- src/main/java/kaptainwutax/itraders/init/InitModel.java
+++ src/main/java/kaptainwutax/itraders/init/InitModel.java
@@ -3,6 +3,7 @@ package kaptainwutax.itraders.init;
 import kaptainwutax.itraders.Traders;
 import kaptainwutax.itraders.item.mesh.FighterEggMesh;
 import kaptainwutax.itraders.item.mesh.MinerEggMesh;
+import kaptainwutax.itraders.item.mesh.SkullNeckMesh;
 import kaptainwutax.itraders.item.mesh.TraderEggMesh;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -19,7 +20,6 @@ public class InitModel {
 		registerSimpleItemModel(InitItem.BIT_5000, 0);
 		registerSimpleItemModel(InitItem.BIT_10000, 0);
 		registerSimpleItemModel(InitItem.EGG_POUCH, 0);
-		registerSimpleItemModel(InitItem.SKULL_NECKLACE, 0);
 		registerSimpleItemModel(InitItem.MAGIC_ORE_DUST, 0);
 		registerSimpleItemModel(InitBlock.ITEM_GRAVE_STONE, 0);
 		registerSimpleItemModel(InitBlock.ITEM_INFUSION_CAULDRON, 0);
@@ -29,6 +29,7 @@ public class InitModel {
 		ModelLoader.setCustomMeshDefinition(InitItem.SPAWN_EGG_TRADER, new TraderEggMesh(InitItem.SPAWN_EGG_TRADER));
 		ModelLoader.setCustomMeshDefinition(InitItem.SPAWN_EGG_FIGHTER, new FighterEggMesh(InitItem.SPAWN_EGG_FIGHTER));
 		ModelLoader.setCustomMeshDefinition(InitItem.SPAWN_EGG_MINER, new MinerEggMesh(InitItem.SPAWN_EGG_MINER));
+		ModelLoader.setCustomMeshDefinition(InitItem.SKULL_NECKLACE, new SkullNeckMesh(InitItem.SKULL_NECKLACE));
 	}
 
 	private static void registerSimpleItemModel(Item item, int metadata) {
