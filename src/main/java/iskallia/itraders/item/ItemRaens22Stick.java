ed3b06eaa7e88669b09cc69bff9b78078532c26b
Anılcan <igoodie@programmer.net>
2019-12-04 00:21:05 +0300

    feat(render): chance textures of Stick of Doom!

--- src/main/java/iskallia/itraders/item/ItemRaens22Stick.java
+++ src/main/java/iskallia/itraders/item/ItemRaens22Stick.java
@@ -3,11 +3,16 @@ package iskallia.itraders.item;
 import iskallia.itraders.Traders;
 import iskallia.itraders.entity.EntityTrader;
 import iskallia.itraders.init.InitItem;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemRaens22Stick extends Item {
 
@@ -15,6 +20,13 @@ public class ItemRaens22Stick extends Item {
         this.setUnlocalizedName(name);
         this.setRegistryName(Traders.getResource(name));
         this.setCreativeTab(InitItem.ITRADERS_TAB);
+        this.setMaxStackSize(1);
     }
 
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+
+        tooltip.add("Raens22's favourite weapon!");
+    }
 }
